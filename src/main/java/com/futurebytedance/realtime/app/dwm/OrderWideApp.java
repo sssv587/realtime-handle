2d9d10bb02c2d291729297702101fdd5cc2a2a1b
sssv587 <1092663295@qq.com>
2022-01-19 00:52:31 +0800

    add province join

--- src/main/java/com/futurebytedance/realtime/app/dwm/OrderWideApp.java
+++ src/main/java/com/futurebytedance/realtime/app/dwm/OrderWideApp.java
@@ -1,6 +1,8 @@
 package com.futurebytedance.realtime.app.dwm;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.futurebytedance.realtime.app.func.DimAsyncFunction;
 import com.futurebytedance.realtime.bean.OrderDetail;
 import com.futurebytedance.realtime.bean.OrderInfo;
 import com.futurebytedance.realtime.bean.OrderWide;
@@ -11,6 +13,7 @@ import org.apache.flink.api.common.functions.RichMapFunction;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.runtime.state.filesystem.FsStateBackend;
 import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.datastream.AsyncDataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
@@ -22,6 +25,8 @@ import org.apache.flink.util.Collector;
 
 import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author yuhang.sun
@@ -143,6 +148,65 @@ public class OrderWideApp {
                     }
                 });
 
+        //TODO 7.关联用户维度
+        SingleOutputStreamOperator<OrderWide> orderWideWithUserDS = AsyncDataStream.unorderedWait(
+                orderWideDataStream,
+                new DimAsyncFunction<OrderWide>("DIM_USER_INFO") {
+                    @Override
+                    public String getKey(OrderWide obj) {
+                        return obj.getUser_id().toString();
+                    }
+
+                    @Override
+                    public void join(OrderWide orderWide, JSONObject dimInfoJson) throws Exception {
+                        //获取用户的生日
+                        String birthday = dimInfoJson.getString("BIRTHDAY");
+
+                        //定义日期转换的工具类
+                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                        //将生日字符串转换为日期对象
+                        Date birthdayDate = sdf.parse(birthday);
+                        //获取生日日期的毫秒数
+                        Long birthdayTs = birthdayDate.getTime();
+
+                        //获取当前时间的毫秒数
+                        Long curTs = System.currentTimeMillis();
+                        //年龄毫秒数转换为年龄
+                        long ageTs = curTs - birthdayTs;
+                        //转换为年龄
+                        long ageLong = ageTs / 1000L / 60L / 60L / 24L / 365L;
+                        int age = (int) ageLong;
+
+                        //将维度中年龄赋值给订单宽表中的字段
+                        orderWide.setUser_age(age);
+                        //将维度中的性别赋值给订单中的宽表
+                        orderWide.setUser_gender(dimInfoJson.getString("GENDER"));
+                    }
+                }, 60, TimeUnit.SECONDS);
+
+        //TODO 8.关联省市维度
+        SingleOutputStreamOperator<OrderWide> orderWideWithProvinceDS = AsyncDataStream.unorderedWait(
+                orderWideWithUserDS,
+                new DimAsyncFunction<OrderWide>("DIM_BASE_PROVINCE") {
+
+                    @Override
+                    public String getKey(OrderWide obj) {
+                        return obj.getProvince_id().toString();
+                    }
+
+                    @Override
+                    public void join(OrderWide orderWide, JSONObject dimInfoJson) {
+                        orderWide.setProvince_name(dimInfoJson.getString("NAME"));
+                        orderWide.setProvince_area_code(dimInfoJson.getString("AREA_CODE"));
+                        orderWide.setProvince_iso_code(dimInfoJson.getString("ISO_CODE"));
+                        orderWide.setProvince_3166_2_code(dimInfoJson.getString("ISO_3166_2"));
+                    }
+                },
+                60,
+                TimeUnit.SECONDS
+        );
+
+        orderDetailDataStream.print(">>>>>>");
 
         env.execute();
     }
